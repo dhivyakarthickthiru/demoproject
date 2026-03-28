c6bd3da91b7836486fdac813b1a93442b80145d4
dhivyakarthickthiru <dhivyathirumugai95@gmail.com>
2026-03-28 20:07:26 +0530

    all the models defined

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,8 +5,15 @@ const { isAuthenticated } = require('../middlewares/auth');
 
 const authRouter =express.Router();
 
+
+//public routes
 authRouter.post('/register',register);
 authRouter.post('/login',login);
+
+
+//protected routes
+
+
 authRouter.get('/getme',isAuthenticated, getme);
 authRouter.post('/logout',isAuthenticated,logout);
 
