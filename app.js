0298a147414bb186fe2cecea8a130a2e879b29d8
dhivyakarthickthiru <dhivyathirumugai95@gmail.com>
2026-03-29 16:18:23 +0530

    node-modules removed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const authRouter=require('./routes/authRoutes');
+
 const cookieparser=require('cookie-parser');
 
 const errorRoute=require('./middlewares/errorRoute');
 const logger=require('./middlewares/logger');
+const companyRouter = require('./routes/companyRoutes');
  
 
 
@@ -16,6 +18,7 @@ app.use(cookieparser());
 app.use(logger);
 
 app.use('/api/v1/auth',authRouter);
+app.use('/api/v1/companies',companyRouter);
 
 
 
